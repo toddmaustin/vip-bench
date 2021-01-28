fdd9ef2ebc8a2694b3389f9f1e38382137f59df8
Todd Austin @ Agita Labs <austin@agitalabs.com>
2021-01-28 13:42:05 -0500

    First draft at a unified VIP benchmarks framework, plus three benchmarks.

---
+++ Makefile
@@ -0,0 +1,66 @@
+include ../config.mk
+
+#
+# END of user-modifiable variables.
+#
+
+ifeq ($(MODE), na)
+TARGET_CFLAGS = $(NA_CFLAGS)
+TARGET_LIBS = $(NA_LIBS)
+TARGET_SIM = $(NA_SIM)
+TARGET_DIFF = $(NA_DIFF)
+TARGET_EXE = $(PROG).na
+else ifeq ($(MODE), do)
+TARGET_CFLAGS = $(DO_CFLAGS)
+TARGET_LIBS = $(DO_LIBS)
+TARGET_SIM = $(DO_SIM)
+TARGET_DIFF = $(DO_DIFF)
+TARGET_EXE = $(PROG).do
+else ifeq ($(MODE), enc)
+TARGET_CFLAGS = $(ENC_CFLAGS)
+TARGET_LIBS = $(ENC_LIBS)
+TARGET_SIM = $(ENC_SIM)
+TARGET_DIFF = $(ENC_DIFF)
+TARGET_EXE = $(PROG).enc
+else
+# default is native
+TARGET_CFLAGS = $(NA_CFLAGS)
+TARGET_LIBS = $(NA_LIBS)
+TARGET_SIM = $(NA_SIM)
+TARGET_DIFF = $(NA_DIFF)
+TARGET_EXE = $(PROG).na
+endif
+
+CFLAGS = -std=c++11 -Wall $(OPT_FLAGS) $(TARGET_CFLAGS) $(LOCAL_CFLAGS)
+LIBS = $(LOCAL_LIBS) $(TARGET_LIBS)
+
+build: $(TARGET_EXE)
+
+%.o: %.cpp
+ifeq ($(MODE), na)
+	$(CXX) $(CFLAGS) -DVIP_NA_MODE -o $@ -c $<
+else ifeq ($(MODE), do)
+	$(CXX) $(CFLAGS) -DVIP_DO_MODE -o $@ -c $<
+else ifeq ($(MODE), enc)
+	$(CXX) $(CFLAGS) -DVIP_ENC_MODE -o $@ -c $<
+else
+	$(error MODE is not defined (add: MODE={na|do|enc}).)
+endif
+
+%.o: %.c
+ifeq ($(MODE), na)
+	$(CC) $(CFLAGS) -DVIP_NA_MODE -o $@ -c $<
+else ifeq ($(MODE), do)
+	$(CC) $(CFLAGS) -DVIP_DO_MODE -o $@ -c $<
+else ifeq ($(MODE), enc)
+	$(CC) $(CFLAGS) -DVIP_ENC_MODE -o $@ -c $<
+else
+	$(error MODE is not defined (add: MODE={na|do|enc}).)
+endif
+
+$(TARGET_EXE): $(OBJS)
+	$(LD) $(CFLAGS) -o $@ $^ $(LIBS)
+
+clean:
+	rm -f $(PROG).na $(PROG).do $(PROG).enc *.o core FOO
+
---
+++ bubble-sort/bubble-sort.cpp
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// include build configuration defines
+#include "../config.h"
+
+#define DATASET_SIZE 256
+VIP_ENCINT data[DATASET_SIZE];
+
+// total swaps executed so far
+unsigned long swaps = 0;
+
+
+void
+print_data(VIP_ENCINT *data, unsigned size)
+{
+  fprintf(stdout, "DATA DUMP:\n");
+  for (unsigned i=0; i < size; i++)
+    fprintf(stdout, "  data[%u] = %d\n", i, VIP_DEC(data[i]));
+}
+
+void
+bubblesort(VIP_ENCINT *data, unsigned size)
+{
+  for (unsigned i=0; i < size-1; i++)
+  {
+#ifndef VIP_DO_MODE
+    bool swapped = false;
+#endif /* !VIP_DO_MODE */
+    for (unsigned j=0; j < size-1; j++)
+    {
+#ifndef VIP_DO_MODE
+      if (data[j] > data[j+1])
+      {
+        VIP_ENCINT tmp = data[j];
+        data[j] = data[j+1];
+        data[j+1] = tmp;
+        swapped = true;
+        swaps++;
+      }
+#else /* VIP_DO_MODE */
+      VIP_ENCBOOL do_swap = data[j] > data[j+1];
+      VIP_ENCINT tmp = data[j];
+      data[j] = VIP_CMOV(do_swap, data[j+1], data[j]);
+      data[j+1] = VIP_CMOV(do_swap, tmp, data[j+1]);
+      swaps++;
+#endif /* VIP_DO_MODE */
+    }
+#ifndef VIP_DO_MODE
+    // done?
+    if (!swapped)
+      break;
+#endif /* !VIP_DO_MODE */
+  }
+}
+
+int
+main(void)
+{
+  // initialize the privacy enhanced execution target
+  VIP_INIT;
+
+  // initialize the pseudo-RNG
+  srand(42);
+  // srand(time(NULL));
+
+  // initialize the array to sort
+  for (unsigned i=0; i < DATASET_SIZE; i++)
+    data[i] = rand();
+  print_data(data, DATASET_SIZE);
+
+  bubblesort(data, DATASET_SIZE);
+  print_data(data, DATASET_SIZE);
+
+  // check the array
+  for (unsigned i=0; i < DATASET_SIZE-1; i++)
+  {
+    if (VIP_DEC(data[i]) > VIP_DEC(data[i+1]))
+    {
+      fprintf(stdout, "ERROR: data is not properly sorted.\n");
+      return -1;
+    }
+  }
+  fprintf(stdout, "INFO: %lu swaps executed.\n", swaps);
+  fprintf(stdout, "INFO: data is properly sorted.\n");
+  return 0;
+}
