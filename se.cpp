cb440ee1001164d3e92f66f3e879b0538474d8bf
Lauren Biernacki <lbiernac@umich.edu>
2021-03-24 13:14:04 -0400

    [Configs] Forgot to include se.cpp file in make

--- se.cpp
+++ se.cpp
@@ -1,18 +1,10 @@
 #ifndef SE_CPP
 #define SE_CPP
-/*
-#include "../functional-library/src/datatypes/enc_lib.h"
-#include "../functional-library/src/datatypes/enc_string.h"
-#include "../functional-library/src/interface/interface.h"
-#include "../functional-library/src/interface/parameters.h"
-*/
+
 #include "se.h"
 
-
-//using namespace enc_lib;
-
 void  SEInit(){
-    enc_lib::setParameters_XOR(0x0505050505050505, 0x0505050505050505, 3022359314);
+    setParameters_XOR(0x0505050505050505, 0x0505050505050505, 3022359314);
 }
 
 #endif
--- se.h
+++ se.h
@@ -5,11 +5,7 @@
 #include "../functional-library/src/interface/interface.h"
 #include "../functional-library/src/interface/parameters.h"
 
-using namespace enc_lib;
 
 void  SEInit();
-//{
-//    setParameters_XOR(0x0505050505050505, 0x0505050505050505, 3022359314);
-//}
 
 #endif
