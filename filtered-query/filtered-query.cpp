fccdb0ac809fe007de69b10970958e1fb429dfc2
Todd Austin @ Agita Labs <austin@agitalabs.com>
2021-11-08 20:26:57 -0500

    More filtered-query development.

--- filtered-query/filtered-query.cpp
+++ filtered-query/filtered-query.cpp
@@ -6,56 +6,110 @@
 
 using namespace std;
 
+#include "../config.h"
 #include "kissdb.h"
 #include "rapidcsv.h"
 
-struct sales_rec_t {
-  unsigned zip;
-  unsigned beds;
-  unsigned baths;
-  unsigned sqft;
-  double price;
+struct salesrec_t {
+  VIP_ENCUINT zip;
+  VIP_ENCUINT beds;
+  VIP_ENCUINT baths;
+  VIP_ENCUINT sqft;
+  VIP_ENCDOUBLE price;
+};
+
+// key format is zip:cnt
+#define KEYFMT(zip,cnt)   (((uint64_t)zip << 32) + (uint64_t)cnt)
+
+void
+print_salesrec(FILE *fd, struct salesrec_t *rec)
+{
+  fprintf(fd, "{ zip:%u, beds:%u, baths:%u, sqft:%u, price:$%.2lf }\n", rec->zip, rec->beds, rec->baths, rec->sqft, rec->price);
+}
+
+vector<unsigned> zipcodes =
+{
+  95603, 95608, 95610, 95614, 95619, 95621, 95623, 95624, 95626, 95628,
+  95630, 95631, 95632, 95633, 95635, 95648, 95650, 95655, 95660, 95661,
+  95662, 95663, 95667, 95670, 95673, 95677, 95678, 95682, 95683, 95690,
+  95691, 95693, 95722, 95726, 95742, 95746, 95747, 95757, 95758, 95762,
+  95765, 95811, 95814, 95815, 95816, 95817, 95818, 95819, 95820, 95821,
+  95822, 95823, 95824, 95825, 95826, 95827, 95828, 95829, 95831, 95832,
+  95833, 95834, 95835, 95838, 95841, 95842, 95843, 95864
 };
 
 void
-print_salesrec(FILE *fd, struct sales_rec_t *rec)
+filtered_query(KISSDB *pdb, enum { op_avg, op_stddev } op, enum { field} field, vector<double> v)
 {
-  fprintf(fd, "{ zip:%u, beds:%u, baths:%u, sqft:%u, price:$%.2lf }\n", rec->zip, rec->beds, rec->baths, rec->sqft, rec->price);
+  struct salesrec_t rec;
+
+  for (unsigned i=0; i < zipcodes.size(); i++)
+  {
+    for (unsigned j=0; ; j++)
+    {
+      uint64_t key = KEYFMT(zipcodes[i], j);
+      if (KISSDB_get(&db, &key, &rec))
+      {
+        // no more entries in this zip code
+        break;
+      }
+      // fprintf(stderr, "<zip,i> = <%u,%u>\n", zipcodes[i], j);
+
+      // process this entry
+      if (filter_fn(&rec))
+      {
+      }
+    }
+  }
 }
 
 int
 main(int argc,char **argv)
 {
-  uint64_t i,j;
-  uint64_t v[8];
   KISSDB db;
   KISSDB_Iterator dbi;
-  char got_all_values[10000];
-  int q;
+  struct salesrec_t rec;
 
-  printf("Reading homesales.csv...\n");
-  rapidcsv::Document doc("homesales.csv");
+  printf("INFO: Generating homesales.db...\n");
 
-  fprintf(stderr, "INFO: num rows: %lu\n", doc.GetRowCount());
-
-  for (unsigned i=0; i < doc.GetRowCount(); i++)
+  if (KISSDB_open(&db, "homesales.db", KISSDB_OPEN_MODE_RWREPLACE, 1024, sizeof(uint64_t), sizeof(rec)))
   {
-    struct sales_rec_t rec;
-    rec.zip = doc.GetCell<unsigned>(0, i);
-    rec.beds = doc.GetCell<unsigned>(1, i);
-    rec.baths = doc.GetCell<unsigned>(2, i);
-    rec.sqft = doc.GetCell<unsigned>(3, i);
-    rec.price = doc.GetCell<double>(4, i);
-    print_salesrec(stderr, &rec);
-  }
-
-  printf("Opening new empty database test.db...\n");
-
-  if (KISSDB_open(&db,"test.db",KISSDB_OPEN_MODE_RWREPLACE,1024,8,sizeof(v))) {
     printf("KISSDB_open failed\n");
     return 1;
   }
 
+  rapidcsv::Document doc("homesales.csv");
+  fprintf(stderr, "INFO: data set size: %lu\n", doc.GetRowCount());
+
+  unsigned currentZip = -1;
+  unsigned zipCnt = 0;
+  uint64_t key = 0;
+  for (unsigned i=0; i < doc.GetRowCount(); i++)
+  {
+    rec.val.zip = doc.GetCell<unsigned>(0, i);
+    rec.val.beds = doc.GetCell<VIP_ENCUINT>(1, i);
+    rec.val.baths = doc.GetCell<VIP_ENCUINT>(2, i);
+    rec.val.sqft = doc.GetCell<VIP_ENCUINT>(3, i);
+    rec.val.price = doc.GetCell<VIP_ENCDOUBLE>(4, i);
+
+    // compute the key val
+    if (currentZip != rec.val.zip)
+    {
+      currentZip = rec.val.zip;
+      zipCnt = 0;
+    }
+    key = KEYFMT(rec.val.zip, zipCnt);
+    zipCnt++;
+
+    // print_salesrec(stderr, &rec);
+
+    if (KISSDB_put(&db, &key, &rec)) {
+      printf("KISSDB_put failed (%u)\n", i);
+      exit(1);
+    }
+  }
+
+#ifdef notdef
   printf("Adding and then re-getting 10000 64-byte values...\n");
 
   for(i=0;i<10000;++i) {
@@ -93,16 +147,30 @@ main(int argc,char **argv)
       }
     }
   }
+#endif /* notdef */
 
-  printf("Closing and re-opening database in read-only mode...\n");
-
+  printf("INFO: Closing and re-opening database in read-only mode...\n");
   KISSDB_close(&db);
 
-  if (KISSDB_open(&db,"test.db",KISSDB_OPEN_MODE_RDONLY,1024,8,sizeof(v))) {
+  if (KISSDB_open(&db,"homesales.db", KISSDB_OPEN_MODE_RDONLY, 1024, sizeof(uint64_t), sizeof(rec))) {
     printf("KISSDB_open failed\n");
-    return 1;
+    exit(1);
   }
 
+
+  // compute average/stddev price for zip code XXXXXX 
+
+  // compute average/stddev price for all zip codes
+
+  // compute average/stddev price/sqft for zip code XXXXXX 
+
+  // compute average/stddev price/sqft for all zip codes
+
+  // find the most expensive zip code in the Sacremento area, by average price/sqrt
+
+  // find the least expensive zip code in the Sacremento area, by average home price
+
+#ifdef notdef
   printf("Getting 10000 64-byte values...\n");
 
   for(i=0;i<10000;++i) {
@@ -137,6 +205,7 @@ main(int argc,char **argv)
       return 1;
     }
   }
+#endif /* notdef */
 
   KISSDB_close(&db);
 
